c7a55add85f5f9a955e926a2044b86ddb6ed5e2e
xasya <xasya89@mail.ru>
2023-03-25 11:36:21 +0300

    Изменена страница ошибки

--- src/pages/not-found-page/NotFoundPage.jsx
+++ src/pages/not-found-page/NotFoundPage.jsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import styles from "./not-found.module.scss"
 
 export default function NotFoundPage(){
+    const error = useRouteError();
+    console.error("Error route", error);
     return(
-        <div>Страница не найдена<br/><Link to="/">Вернуться на главную</Link></div>
+        <div className={styles.notFoundBody}>
+            <h1 className={styles.notFoundH1}>Whoops!</h1>
+            <p className={styles.notFoundP}>
+                {error.message}
+            </p>
+            <p className={styles.notFoundP}>
+                <Link style={{color: "red"}} to="/">Вернуться на главную</Link>
+            </p>
+        </div>
     )
 }
